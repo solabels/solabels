04755432b19fb6e96e361b4b90e644d2af0dc0df
yordanlasonov <yordan.lasonov@gmail.com>
2019-07-26 11:12:01 +0300

    folder fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,16 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // {
-    //   resolve: `gatsby-source-prismic-graphql`,
-    //   options: {
-    //     repositoryName: "solarlabel",
-    //     accessToken:
-    //       "MC5YVFhFRFJBQUFDSUFOTlA4.Qnbvv70IdO-_vQ09En1k77-9PXLvv70xa--_ve-_ve-_vVDvv71H77-9MAl177-977-9eE3vv70",
-    //     path: "/", // (optional, default: /preview)
-    //     previews: true, // (optional, default: false)
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-prismic-graphql`,
+      options: {
+        repositoryName: "solarlabel",
+        accessToken:
+          "MC5YVFhFRFJBQUFDSUFOTlA4.Qnbvv70IdO-_vQ09En1k77-9PXLvv70xa--_ve-_ve-_vVDvv71H77-9MAl177-977-9eE3vv70",
+        path: "/", // (optional, default: /preview)
+        previews: true, // (optional, default: false)
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
