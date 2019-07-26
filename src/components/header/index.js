0a12144c7c484bb7f27b8c318cadaa7179c44c16
yordanlasonov <yordan.lasonov@gmail.com>
2019-07-26 11:17:05 +0300

    trying to fix build

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import Skew from "../Skew"
+import Skew from "../skew"
 
 const Header = ({ siteTitle }) => (
   <Skew
