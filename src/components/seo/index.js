0b366c1d38b137eb95fab1c7556461cf8b2455a8
yordanlasonov <yordan.lasonov@gmail.com>
2019-12-06 15:29:15 +0200

    meta tag from g suite

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -23,6 +23,10 @@ function SEO() {
           content="A proven solution for your custom solar labeling needs - from label design to printing, sorting and shipping. Whatever your solar cable label and equipment identification needs are, we have the resources to interpret, generate, print, sort and deliver them in many customizable packages. Our products are thoughtfully created to accelerate your teams' install, reduce on-site resources and decrease the overall cost of electrical installation."
         />
         <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta
+          name='google-site-verification'
+          content='V-W3_PVVxuXSkuRE-sUMxyCcfAomFiqXYPqioHPJ4aI'
+        />
         <link href='https://solabels.com/' rel='canonical' />
         <link href='https://solabels.com/' rel='home' />
         <link type='text/plain' href='https://solabels.com/' rel='author' />
